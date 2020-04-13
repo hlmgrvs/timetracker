79631cebd458329b1b339a1469527932a598a6c4
Vladislavs Holmogorovs <40771961+hlmgrvs@users.noreply.github.com>
2020-04-13 11:45:13 +0200

    homeView i18n

--- src/components/Home/HomeView.js
+++ src/components/Home/HomeView.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import HomeViewStyles from './HomeViewStyles';
+import i18n from '../../i18n/i18n'
 
 const HomeView = () => {
     return (
         <View style={[{ flex: 1 }, HomeViewStyles.homeViewContainer]} >
             <View style={{ flex: 1 }}>
-                <Text style={HomeViewStyles.welcomeHeader}>Good Morning!</Text>
+    <Text style={HomeViewStyles.welcomeHeader}>{i18n.HOME.WELCOME_HEADER}</Text>
             </View>
             <View style={{ flex: 2 }}>
                 <TouchableOpacity style={HomeViewStyles.mainActionButton} onPress={() => {
                     console.log("button pressed");
                 }}>
-                    <Text style={HomeViewStyles.mainActionButtonText}>START</Text>
+                    <Text style={HomeViewStyles.mainActionButtonText}>{i18n.HOME.START}</Text>
                 </TouchableOpacity>
             </View>
 
